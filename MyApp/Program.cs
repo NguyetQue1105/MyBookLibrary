88344f0e6c9b9b9df81eef25b964eaec216decb9
NguyetQue1105 <tracthanhnguyetque@gmail.com>
2020-11-16 08:18:02 +0700

    first commit

---
+++ MyApp/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyBookLibrary;
+
+namespace MyApp
+{
+    class Program
+    {
+        public static void menu()
+        {
+            Console.WriteLine("***   MENU   ***");
+            Console.WriteLine("1. Add new book");
+            Console.WriteLine("2. Update a book");
+            Console.WriteLine("3. Delete a book");
+            Console.WriteLine("4. List all book");
+            Console.WriteLine("5. Quit");
+        }
+        static void Main(string[] args)
+        {
+            string ID;
+            string Name;
+            string Publisher;
+            string Price;
+            int choice;
+
+            do
+            {
+                menu();
+                choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Input Book's ID: ");
+                        ID = Console.ReadLine();
+                        Console.WriteLine("Input Book's name: ");
+                        Name = Console.ReadLine();
+                        Console.WriteLine("Input Book's Publisher: ");
+                        Publisher = Console.ReadLine();
+                        Console.WriteLine("Input Book's price: ");
+                        Price = Console.ReadLine();
+                        MyBookLibrary.MyBookLibrary.AddBook(ID, Name, Publisher, Price);
+                        break;
+                    case 2:
+                        Console.WriteLine("Input Book's ID to update: ");
+                        ID = Console.ReadLine();
+                        MyBookLibrary.MyBookLibrary.UpdateBook(ID);
+                        break;
+                    case 3:
+                        Console.WriteLine("Input Book's ID to delete: ");
+                        ID = Console.ReadLine();
+                        MyBookLibrary.MyBookLibrary.Deletebook(ID);
+                        break;
+                    case 4:
+                        MyBookLibrary.MyBookLibrary.ListBook();
+                        break;
+                    case 5:
+                        Console.WriteLine("Good bye");
+                        Console.ReadLine();
+                        break;
+                    default: break;
+                }
+            } while (choice >= 1 && choice <= 4);
+        }
+    }
+}
---
+++ MyBookLibrary/MyBookLibrary.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBookLibrary
+{
+    public class MyBookLibrary
+    {
+        private string ID;
+
+        public string getID
+        {
+            get { return ID; }
+            set { ID = value; }
+        }
+
+        private string Name;
+
+        public string getName
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+
+        private string Publisher;
+
+        public string getPublisher
+        {
+            get { return Publisher; }
+            set { Publisher = value; }
+        }
+
+        private string Price;
+
+        public string getPrice
+        {
+            get { return Price; }
+            set { Price = value; }
+        }
+
+        public MyBookLibrary()
+        {
+            ID = null;
+            Name = null;
+            Publisher = null;
+            Price = null;
+        }
+
+        public MyBookLibrary(string ID, string Name, string Publisher, string Price)
+        {
+            this.ID = ID;
+            this.Name = Name;
+            this.Publisher = Publisher;
+            this.Price = Price;
+        }
+
+        static List<MyBookLibrary> BookList = new List<MyBookLibrary>();
+
+        public static MyBookLibrary SearchBook(string ID)
+        {
+            MyBookLibrary Book = null;
+            foreach (var book in BookList)
+            {
+                if(ID == book.ID)
+                {
+                    Book = book;
+                }
+            }
+            return Book;
+
+        }
+
+        public static void AddBook (string ID, string Name, string Publisher, string Price)
+        {
+            if (SearchBook(ID)==null)
+            {
+                BookList.Add(new MyBookLibrary(ID, Name, Publisher, Price));
+                Console.WriteLine("Them sach thanh cong");
+            }
+        }
+
+        public static void UpdateBook(string ID)
+        {
+            MyBookLibrary book = SearchBook(ID);
+            if (book != null)
+            {
+                Console.WriteLine("Ma sach: " + book.ID);
+                Console.WriteLine("Nhap ten sach: ");
+                book.Name = Console.ReadLine();
+                Console.WriteLine("Nhap nha san xuat: ");
+                book.Publisher = Console.ReadLine();
+                Console.WriteLine("Nhap gia sach: ");
+                book.Price = Console.ReadLine();
+                Console.WriteLine("Cap nhat thong tin sach thanh cong");
+            }
+            else
+            {
+                Console.WriteLine("Khong tim thay sach co ma: " + ID);
+            }
+        }
+
+        public static void Deletebook(string ID)
+        {
+            MyBookLibrary book = SearchBook(ID);
+            if (book != null)
+            {
+                BookList.Remove(book);
+                Console.WriteLine("Xoa sach thanh cong");
+            }
+        }
+
+        public void BookInfo(MyBookLibrary book)
+        {
+            Console.WriteLine("Book's ID: " + book.ID);
+            Console.WriteLine("Book's Name: " + book.Name);
+            Console.WriteLine("Book's Publisher: " + book.Publisher);
+            Console.WriteLine("Book's Price: " + book.Price);
+        }
+
+        public static void ListBook()
+        {
+            Console.WriteLine("Book's List");
+            foreach (var book in BookList)
+            {
+                book.BookInfo(book);
+                Console.ReadLine();
+            }
+        }
+    }
+}
